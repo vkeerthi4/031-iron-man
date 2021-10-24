7e20afce03232fcab5168637cd7459eaf4daf13e
vkeerthi4 <90127122+vkeerthi4@users.noreply.github.com>
2021-10-24 19:35:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var ironman;
+
+var bg, backgroundImg;
+
+function preload() {
+  backgroundImg = loadImage("images/bg.jpg");
+  ironmanimg=loadImage("images/iron.png")
+}
+
+function setup() {
+  createCanvas(1000, 600);
+  bg = createSprite(580,300);
+  bg.addImage(backgroundImg);
+  bg.scale=1;
+
+  ironman=createSprite(50,500);
+  ironman.addImage(ironmanimg)
+  ironman.scale=0.4; 
+}
+
+function draw() {
+  ironman.x= mouseX;
+  ironman.y= mouseY; 
+ 
+    
+    drawSprites();}
